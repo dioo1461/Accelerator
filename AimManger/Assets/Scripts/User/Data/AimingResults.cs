1e1e02ac43449fdeed34b638d604e21013857dc6
dioo1461 <dioo1461@naver.com>
2022-10-04 21:29:54 +0900

    first commit

---
+++ AimManger/Assets/Scripts/User/Data/AimingResults.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserAimingResults : MonoBehaviour
+{
+	public float accuracy;
+	
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ AimManger/Assets/Scripts/User/Data/TargetData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetData : MonoBehaviour
+{
+	float size;
+	float spawnAngle;
+	float lifeTime;
+
+	const float MAX_SIZE = 1;
+	const float MIN_SIZE = 0.5f;
+	const float MAX_LIFETIME = 0.45f;
+	const float MIN_LIFETIME = 0.1f;
+
+}
